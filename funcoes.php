de91048bff1d9b1519183605abe464fbba2e9734
Ga2236 <55792432+Ga2236@users.noreply.github.com>
2019-09-25 11:29:57 -0300

    Create funcoes.php

---
+++ funcoes.php
@@ -0,0 +1,12 @@
+<?php
+
+/*function conecta( $host , $usuario, $senha, $banco, $sql )
+{
+	error_reporting(0);
+    $conexao = mysql_connect($host,$usuario,$senha,$banco) or die("Erro na Conexao"); 
+	$db = mysql_select_db($banco,$conexao) or die("Erro na selecao do banco");
+	$resultado = mysql_query($sql,$conexao) or die("Erro na query");
+	mysql_close($conexao);
+	return $resultado;
+}
+*/
